86e97c3ecc31fdb4343b925bab954404d5c3373d
Tyler Ke <tdkeytalt@gmail.com>
2025-07-27 15:55:28 -0500

    added button-counter component and made email in contact-us component dynamic

--- vue-components-demo/src/components/ContactUs.vue
+++ vue-components-demo/src/components/ContactUs.vue
@@ -1,6 +1,18 @@
 <template>
    <div class="">
       <h1>Contact Us</h1>
-      <p>Please send us an email at fakeemail.gmail.com</p>
+      <p>Please send us an email at {{ email }}</p>
    </div>
 </template>
+
+<script>
+import {ref} from "vue";
+
+export default{
+   setup() {
+      const email = ref("fakeemail2@gmail.com")
+
+      return { email };
+   }
+}
+</script>
