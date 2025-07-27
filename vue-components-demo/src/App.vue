107f66c441079362d7afaebfa238c4f8af8b1208
Tyler Ke <tdkeytalt@gmail.com>
2025-07-27 17:22:14 -0500

    added ownername prop and v-bound it to contactsOwner variable

--- vue-components-demo/src/App.vue
+++ vue-components-demo/src/App.vue
@@ -1,14 +1,14 @@
 <template>
    <div class="container">
       <div>{{ message }}</div>
-      <Contact name="Tyler" phone="123-123-1234"></Contact>
+      <Contact name="Tyler" phone="123-123-1234" :ownername="contactsOwner"></Contact>
    </div>
 </template>
 
 <script setup>
-   import ButtonCounter from "./components/ButtonCounter.vue"
    import Contact from "./components/Contact.vue"
    const message = "Hello Vue";
+   const contactsOwner = "gooogle";
 </script>
 
 <style>
