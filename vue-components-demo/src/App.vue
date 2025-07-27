ea780955742a1242b36cc41e14c870f6414fdbf4
Tyler Ke <tdkeytalt@gmail.com>
2025-07-27 15:42:47 -0500

    created message variable to display in template

--- vue-components-demo/src/App.vue
+++ vue-components-demo/src/App.vue
@@ -1,3 +1,19 @@
 <template>
-   <div>Hello World</div>
+   <div>{{ message }}</div>
 </template>
+
+<script>
+
+export default{
+   setup() {
+      const message = "Hello Vue"
+
+      return {message};
+   },
+};
+
+</script>
+
+<style>
+
+</style>
