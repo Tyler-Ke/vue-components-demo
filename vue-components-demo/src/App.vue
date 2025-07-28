3cca1daa7534702aa1fcddabf0d332c42d29776a
Tyler Ke <tdkeytalt@gmail.com>
2025-07-28 16:51:49 -0500

    v-binded ownername to a text input and added a button that modifies itself when the contact is favorite

--- vue-components-demo/src/App.vue
+++ vue-components-demo/src/App.vue
@@ -1,6 +1,7 @@
 <template>
    <div class="container">
       <div>{{ message }}</div>
+      Contact Owner Name: <input v-model="ownerName" />
       <div class="row">
          <div class="col-12" v-for="contact in contacts" :key="contact.name">
             <Contact
@@ -24,13 +25,13 @@ const contacts = reactive([
    {
       name: "Tyler",
       phone: 1231231234,
-      ownerName: ownerName.value,
+      ownerName: ownerName,
       isFavorite: false,
    },
    {
       name: "Vincent",
       phone: 5554448888,
-      ownerName: ownerName.value,
+      ownerName: ownerName,
       isFavorite: true,
    },
    {
--- vue-components-demo/src/components/Contact.vue
+++ vue-components-demo/src/components/Contact.vue
@@ -1,15 +1,18 @@
 <template>
-   <div class="bg-info rounded p-4 m-2">
+   <div class="bg-info rounded p-1 pb-4 m-2">
       <div class="row">
          <div class="col-12">
             <h3>Name: {{ name }}</h3>
             <p>Email: {{ email }}</p>
             <p>Phone: {{ phone }}</p>
-            <p class="float-end small" v-if="ownername !== ''">
-               This contact belongs to: {{ ownername }}
-            </p>
+         </div>
+         <div class="col-3">
+            <button :class="[isFavorite ? 'btn btn-warning' : 'btn btn-success']">{{ isFavorite ? "Remove from" : "Add to" }} Favorite</button>
          </div>
       </div>
+      <span class="float-end small" v-if="ownername !== ''">
+         This contact belongs to: {{ ownername }}
+      </span>
    </div>
 </template>
 
