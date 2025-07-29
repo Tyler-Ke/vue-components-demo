1c23a545c61c65eb50add438e7b9b24f15be7fcd
Tyler Ke <tdkeytalt@gmail.com>
2025-07-29 17:36:07 -0500

    added an add contact component

--- vue-components-demo/src/App.vue
+++ vue-components-demo/src/App.vue
@@ -1,25 +1,33 @@
 <template>
-   <div class="container">
-      <div>{{ message }}</div>
-      Contact Owner Name: <input v-model="ownerName" />
-      <div class="row">
-         <div class="col-12" v-for="contact in contacts" :key="contact.name">
-            <Contact
-               :name="contact.name"
-               :phone="contact.phone"
-               :ownername="contact.ownerName"
-               :email="contact.email"
-               :isFavorite="contact.isFavorite"
-               @update-favorite="contact.isFavorite = onUpdateFavorite($event, contact.phone)"
-            ></Contact>
+   <div class="bg-black text pt-3">
+      <div class="container">
+         <div class="text-white float-end">
+            Contact Owner Name: <input v-model="ownerName" />
+         </div>
+         <br /><br />
+         <AddContact></AddContact>
+         <div class="row">
+            <div class="col-12" v-for="contact in contacts" :key="contact.name">
+               <Contact
+                  :name="contact.name"
+                  :phone="contact.phone"
+                  :ownername="contact.ownerName"
+                  :email="contact.email"
+                  :isFavorite="contact.isFavorite"
+                  @update-favorite="
+                     contact.isFavorite = onUpdateFavorite($event, contact.phone)
+                  "
+               ></Contact>
+            </div>
          </div>
       </div>
    </div>
 </template>
 
 <script setup>
-import {reactive, ref} from "vue";
+import { reactive, ref } from "vue";
 import Contact from "./components/Contact.vue";
+import AddContact from "./components/AddContact.vue";
 const message = "Hello Vue";
 const ownerName = ref("Alphabet Inc");
 const contacts = reactive([
@@ -45,7 +53,7 @@ const contacts = reactive([
 ]);
 
 function onUpdateFavorite(oldValueFromChildComponent, phoneNumberFromParent) {
-   console.log(oldValueFromChildComponent)
+   console.log(oldValueFromChildComponent);
    return !oldValueFromChildComponent.isFavorite;
 }
 </script>
