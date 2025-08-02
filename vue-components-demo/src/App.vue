370ecb4eca5603cd898c4c6aaeffbf722d02006c
Tyler Ke <tdkeytalt@gmail.com>
2025-08-02 10:49:40 -0500

    proved that slot belongs to the parent component

--- vue-components-demo/src/App.vue
+++ vue-components-demo/src/App.vue
@@ -2,10 +2,19 @@
    <LuckyNumberParent>
       <p class="pt-2">We have two versions for picking a lucky number</p>
       <p>Click the button to toggle between the two versions</p>
+      <button @click="showMessage">What will we learn?</button>
+      <h4 class="text-success">{{ message }}</h4>
       <hr />
    </LuckyNumberParent>
 </template>
 
 <script setup>
-   import LuckyNumberParent from './components/LuckyNumberParent.vue';
+import { ref } from "vue";
+import LuckyNumberParent from "./components/LuckyNumberParent.vue";
+
+const message = ref("");
+
+function showMessage() {
+   message.value = "learn slots";
+}
 </script>
