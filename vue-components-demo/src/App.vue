e0e1df261e5bce5a10a2f9b99c9613fc472ebc80
Tyler Ke <tdkeytalt@gmail.com>
2025-08-02 19:17:03 -0500

    used composible in several files and created props for composible

--- vue-components-demo/src/App.vue
+++ vue-components-demo/src/App.vue
@@ -2,11 +2,14 @@
    <button @click="increment">Increment</button>
    <button @click="decrement">Decrement</button>
    <p>{{ count }}</p>
+   <br /><br />
+   <ButtonCounter></ButtonCounter>
 </template>
 
 <script setup>
 import { useCounter } from './utility/useCounter';
+import ButtonCounter from './components/ButtonCounter.vue';
 
-const {count, increment, decrement} = useCounter();
+const {count, increment, decrement} = useCounter(100, 10);
 
 </script>
--- vue-components-demo/src/components/ButtonCounter.vue
+++ vue-components-demo/src/components/ButtonCounter.vue
@@ -1,7 +1,12 @@
 <template>
-   Counter
+   <button @click="increment">++</button>
+   <span>{{count}}</span>
+   <button @click="decrement">--</button>
 </template>
 
 <script setup>
+import { useCounter } from '../utility/useCounter';
+
+const {count, increment, decrement} = useCounter();
 
 </script>
